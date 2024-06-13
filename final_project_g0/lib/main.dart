8d31b781c523d6ccf2b358cd96d13af4d1991eda
Lithashl <90357502+Lithashl@users.noreply.github.com>
2024-06-13 16:42:43 +0700

    Add files via upload

--- final_project_g0/lib/main.dart
+++ final_project_g0/lib/main.dart
@@ -1,7 +1,13 @@
-import 'package:firebase/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:fp_1/pages/add_hotel.dart';
+import 'package:fp_1/pages/add_room.dart';
+import 'package:fp_1/pages/auth.dart';
+import 'package:fp_1/pages/booking_page.dart';
+import 'package:fp_1/pages/login.dart';
+import 'package:fp_1/pages/register.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,11 +20,27 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      routes: {
+        '/register' :(context) => RegisterPage(),
+        '/login' :(context) => LoginPage(),
+        '/add_hotel' :(context) => AddHotelPage(),
+        '/add_room' :(context) => AddRoomPage(hotelID: '',),
+        '/booking_page' :(context) => BookingsPage(hotelID: '', roomID: '',),
+
+
+
+      },
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: AuthPage(),
     );
   }
 }
+
